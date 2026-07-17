926792f93c32b6a0258590d7b08cde83e6342d9c
Mahesh-xr <mahesh.572xr@gmail.com>
2026-07-17 16:03:25 +0530

    feat: Update hands_on_5.py to implement alert message handling and button click automation in Selenium

--- Week 5/Module 7 Selinium Basics/automation_scripts/hands_on_5.py
+++ Week 5/Module 7 Selinium Basics/automation_scripts/hands_on_5.py
@@ -1,3 +1,40 @@
+# # # from selenium import webdriver
+# # # from selenium.webdriver.common.by import By
+# # # from selenium.webdriver.chrome.service import Service
+# # # from webdriver_manager.chrome import ChromeDriverManager
+
+# # # service = Service(ChromeDriverManager().install())
+
+# # # driver = webdriver.Chrome(service=service)
+
+# # # driver.get(
+# # #     "https://www.lambdatest.com/selenium-playground/simple-form-demo"
+# # # )
+
+# # # # CSS Selector using ID
+# # # element1 = driver.find_element(
+# # #     By.CSS_SELECTOR,
+# # #     "#user-message"
+# # # )
+
+# # # # CSS Selector using attribute
+# # # element2 = driver.find_element(
+# # #     By.CSS_SELECTOR,
+# # #     "[name='user-message']"
+# # # )
+
+# # # # CSS Selector using parent-child
+# # # element3 = driver.find_element(
+# # #     By.CSS_SELECTOR,
+# # #     "div > input"
+# # # )
+
+# # # print(element1)
+# # # print(element2)
+# # # print(element3)
+
+# # # driver.quit()
+
 # # from selenium import webdriver
 # # from selenium.webdriver.common.by import By
 # # from selenium.webdriver.chrome.service import Service
@@ -8,36 +45,33 @@
 # # driver = webdriver.Chrome(service=service)
 
 # # driver.get(
-# #     "https://www.lambdatest.com/selenium-playground/simple-form-demo"
+# #     "https://www.lambdatest.com/selenium-playground/checkbox-demo"
 # # )
 
-# # # CSS Selector using ID
-# # element1 = driver.find_element(
-# #     By.CSS_SELECTOR,
-# #     "#user-message"
+# # # Locate using exact text
+# # label = driver.find_element(
+# #     By.XPATH,
+# #     "//label[text()='Option 1']"
 # # )
 
-# # # CSS Selector using attribute
-# # element2 = driver.find_element(
-# #     By.CSS_SELECTOR,
-# #     "[name='user-message']"
+# # print(label.text)
+
+# # # Locate all labels containing "Option"
+# # labels = driver.find_elements(
+# #     By.XPATH,
+# #     "//label[contains(text(),'Option')]"
 # # )
 
-# # # CSS Selector using parent-child
-# # element3 = driver.find_element(
-# #     By.CSS_SELECTOR,
-# #     "div > input"
-# # )
-
-# # print(element1)
-# # print(element2)
-# # print(element3)
+# # for item in labels:
+# #     print(item.text)
 
 # # driver.quit()
 
 # from selenium import webdriver
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 # from webdriver_manager.chrome import ChromeDriverManager
 
 # service = Service(ChromeDriverManager().install())
@@ -45,25 +79,23 @@
 # driver = webdriver.Chrome(service=service)
 
 # driver.get(
-#     "https://www.lambdatest.com/selenium-playground/checkbox-demo"
+#     "https://www.lambdatest.com/selenium-playground/bootstrap-alert-messages-demo"
 # )
 
-# # Locate using exact text
-# label = driver.find_element(
-#     By.XPATH,
-#     "//label[text()='Option 1']"
+# driver.find_element(
+#     By.ID,
+#     "autoclosable-btn-success"
+# ).click()
+
+# alert = WebDriverWait(driver, 10).until(
+#     EC.visibility_of_element_located(
+#         (By.CSS_SELECTOR, ".alert-success")
+#     )
 # )
 
-# print(label.text)
+# assert "successfully" in alert.text.lower()
 
-# # Locate all labels containing "Option"
-# labels = driver.find_elements(
-#     By.XPATH,
-#     "//label[contains(text(),'Option')]"
-# )
-
-# for item in labels:
-#     print(item.text)
+# print(alert.text)
 
 # driver.quit()
 
@@ -82,19 +114,12 @@ driver.get(
     "https://www.lambdatest.com/selenium-playground/bootstrap-alert-messages-demo"
 )
 
-driver.find_element(
-    By.ID,
-    "autoclosable-btn-success"
-).click()
-
-alert = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located(
-        (By.CSS_SELECTOR, ".alert-success")
+button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable(
+        (By.ID, "autoclosable-btn-success")
     )
 )
 
-assert "successfully" in alert.text.lower()
-
-print(alert.text)
+button.click()
 
 driver.quit()
