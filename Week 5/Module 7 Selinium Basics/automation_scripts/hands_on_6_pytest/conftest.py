f5ffaffc8c70106c9d004934de66afb8b11087d5
Mahesh-xr <mahesh.572xr@gmail.com>
2026-07-18 11:38:54 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Week 5/Module 7 Selinium Basics/automation_scripts/hands_on_6_pytest/conftest.py
@@ -0,0 +1,52 @@
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+
+# @pytest.fixture(scope="session")
+# def driver():
+
+#     service = Service(
+#         ChromeDriverManager().install()
+#     )
+
+#     driver = webdriver.Chrome(service=service)
+
+#     yield driver
+
+#     driver.quit()
+
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(scope="function")
+def driver(request):
+
+    driver = webdriver.Chrome()
+
+    # Make the driver available to the hook
+    request.node.driver = driver
+
+    yield driver
+
+    driver.quit()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+
+    outcome = yield
+
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+
+        driver = getattr(item, "driver", None)
+
+        if driver:
+            test_name = item.name
+            driver.save_screenshot(
+                f"{test_name}_failure.png"
+            )
+            
